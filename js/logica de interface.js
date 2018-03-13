5fb794a4cb6314b848346369502388e95ba0fe0c
Samir Mejia <samir4mt@gmail.com>
2018-03-12 22:11:00 -0500

    Login Funcionando

--- js/logica de interface.js
+++ js/logica de interface.js
@@ -1,18 +1,38 @@
-document.querySelector('#btnIngresar').addEventListener('click',iniciarSesion);
+// document.getElementById('#tnIngresar').addEventListener('click',iniciarSesion());
 
-
+usuariosRegistrados = {
+	"hernan" : "1234",
+	"invitado" : "1234",
+};
 
 function iniciarSesion(){
 
-var sCorreo = '' ;
-var sContrasenna = '' ;
-var bAcceso = false;
+	// var usuarioIngresado = '' ;
+	// var contrasenaIngresada = '' ;
+	var bAcceso = false;
 
-sCorreo = document.querySelector('#txtCorreo').value;
-sContrasenna = document.querySelector('#txtContrasenna').value;
+	usuarioIngresado = document.getElementById('usuario').value;
+	contrasenaIngresada = document.getElementById('contrasena').value;
 
-bAcceso = validarCredeniales(sCorreo,sContrasenna);
-console.log(bAcceso);
+	bAcceso = validarCredeniales(usuarioIngresado,contrasenaIngresada);
+	console.log(bAcceso);
 
+	if (bAcceso){
+		location.href ="cliente.html";
+	}
 
+}
+
+function validarCredeniales(usuarioIngresado,contrasenaIngresada){
+	
+	if (usuariosRegistrados[usuarioIngresado]){
+		if (usuariosRegistrados[usuarioIngresado] == contrasenaIngresada){
+			return true;	
+		}else{
+			alert("La contraseña ingresada es incorrecta. Intente de nuevo.");
+		}
+	}else{
+		alert("El usuario ingresado no se encuentra registrado.");
+	}
+	return false;
 }
